6099f16932985004b3a2ab53579e8211b4af7f94
Hiko <hikomoon@outlook.com>
2025-06-03 13:10:43 +0800

    exercise

---
+++ exercise/1.3.rkt
@@ -0,0 +1,7 @@
+(define <= (lambda (a b) (or (= a b) (< a b))))
+(define (sum-of-squares a b)
+  (+ (* a a) (* b b)))
+(define (f a b c)
+    (cond ((and (<= a b) (<= a c)) (sum-of-squares b c))
+          ((and (<= b a) (<= b c)) (sum-of-squares a c))
+          ((and (<= c a) (<= c b)) (sum-of-squares a b))))
