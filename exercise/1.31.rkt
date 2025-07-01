189795c6df660fb5050d4869f68c2ac086d3d777
Hiko <hikomoon@outlook.com>
2025-07-01 17:22:41 +0800

    exercise

--- exercise/1.31.rkt
+++ exercise/1.31.rkt
@@ -17,7 +17,14 @@
     (if (> a b) 1
         (* (term a) (product term (next a) next b))))
     
+; (define (identity x) x)
+; (define (inc x) (+ x 1))
+; (product identity 1 inc 4)
 
-(define (identity x) x)
-(define (inc x) (+ x 1))
-(product identity 1 inc 4)
+
+(define (pi)
+    (define (term x) (/ (* (+ x 1) (- x 1)) (sq x)))
+    (define (next a) (+ a 2))
+    (exact->inexact (* 4 (product term 3 next 10000))))
+
+(pi)
