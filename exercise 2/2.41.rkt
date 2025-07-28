b9092343f10d7ee102f1808c8d799a338cd92f73
Hiko <hikomoon@outlook.com>
2025-07-28 11:43:02 +0800

    exercise

---
+++ exercise 2/2.41.rkt
@@ -0,0 +1,31 @@
+#lang racket
+(require sicp)
+(require "../utils/index.rkt")
+
+(define (flatmap proc s)
+    (accumulate append nil (map proc s)))
+
+(define (enumerate-interval i j)
+    (if (= j i)
+        (list j)
+        (append (list j) (enumerate-interval i (- j 1)))))
+
+(define (unique-pairs n)
+    (flatmap (lambda (j) 
+            (if (= j 1)
+                nil
+                (map (lambda (i) (list i j))
+                    (enumerate-interval 1 (- j 1)))))
+        (enumerate-interval 1 n)))
+
+; (display (unique-pairs 4))
+; (newline)
+
+(define (unique-triples n)
+    (if (= n 3)
+        (list (list 1 2 3))
+        (append (map (lambda (p) (append (list n) p)) (unique-pairs (- n 1)))
+            (unique-triples (- n 1)))))
+
+(display (unique-triples 5))
+    
