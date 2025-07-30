e31a6b44b84fdd5260310e883f63d93ed74d0aab
Hiko <hikomoon@outlook.com>
2025-07-30 09:53:31 +0800

    exercise

---
+++ exercise 2/2.49.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(require sicp)
+(require "../utils/index.rkt")
+(require "../utils/listoperations.rkt")
+; (require "./2.48.rkt")
+
+; (define (segments->painter segment-list)
+;     (lambda (frame)
+;         (for-each
+;             (lambda (segment)
+;                 (draw-line
+;                     ((frame-coord-map frame) (start-segment segment))
+;                     ((frame-coord-map frame) (end-segment segment))))
+;             segment-list)))
+
+
+; (segments->painter (list (make-segment 0 0) (make-segment 1 1)))
+
+略。
