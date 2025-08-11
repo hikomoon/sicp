0d7685ba3b60542b7d88045b36d886b2a5dd60cd
Hiko <hikomoon@outlook.com>
2025-08-11 15:32:35 +0800

    exercise

---
+++ exercise 2/2.55.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(require sicp)
+(require "../utils/index.rkt")
+(require "../utils/listoperations.rkt")
+
+(display 'abracadabra)
+(display ''abracadabra)
+(display '(quote abracadabra))
+(quote (quote abracadabra))
